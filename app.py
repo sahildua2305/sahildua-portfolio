28dc07fcb9b32ce85b4682e57a251be9957f0d0b
Sahil Dua <sahildua2305@gmail.com>
2024-11-17 17:19:41 +0100

    Disable all unused endpoints

--- app.py
+++ app.py
@@ -20,68 +20,68 @@ def index():
     return render_template('home.html', common=common)
 
 
-@app.route('/timeline')
-def timeline():
-    timeline = get_static_json("static/files/timeline.json")
-    return render_template('timeline.html', common=common, timeline=timeline)
+# @app.route('/timeline')
+# def timeline():
+#     timeline = get_static_json("static/files/timeline.json")
+#     return render_template('timeline.html', common=common, timeline=timeline)
 
 
-@app.route('/reading')
-def reading():
-    data = get_static_json("static/files/reading.json")
-    return render_template('reading.html', common=common, data=data)
+# @app.route('/reading')
+# def reading():
+#     data = get_static_json("static/files/reading.json")
+    # return render_template('reading.html', common=common, data=data)
 
 
-@app.route('/projects')
-def projects():
-    data = get_static_json("static/projects/projects.json")['projects']
-    data.sort(key=order_projects_by_weight, reverse=True)
+# @app.route('/projects')
+# def projects():
+#     data = get_static_json("static/projects/projects.json")['projects']
+#     data.sort(key=order_projects_by_weight, reverse=True)
 
-    tag = request.args.get('tags')
-    if tag is not None:
-        data = [project for project in data if tag.lower() in [project_tag.lower() for project_tag in project['tags']]]
+#     tag = request.args.get('tags')
+#     if tag is not None:
+#         data = [project for project in data if tag.lower() in [project_tag.lower() for project_tag in project['tags']]]
 
-    return render_template('projects.html', common=common, projects=data, tag=tag)
-
-
-@app.route('/experiences')
-def experiences():
-    experiences = get_static_json("static/experiences/experiences.json")['experiences']
-    experiences.sort(key=order_projects_by_weight, reverse=True)
-    return render_template('projects.html', common=common, projects=experiences, tag=None)
+#     return render_template('projects.html', common=common, projects=data, tag=tag)
 
 
-def order_projects_by_weight(projects):
-    try:
-        return int(projects['weight'])
-    except KeyError:
-        return 0
+# @app.route('/experiences')
+# def experiences():
+#     experiences = get_static_json("static/experiences/experiences.json")['experiences']
+#     experiences.sort(key=order_projects_by_weight, reverse=True)
+#     return render_template('projects.html', common=common, projects=experiences, tag=None)
 
 
-@app.route('/projects/<title>')
-def project(title):
-    projects = get_static_json("static/projects/projects.json")['projects']
-    experiences = get_static_json("static/experiences/experiences.json")['experiences']
+# def order_projects_by_weight(projects):
+#     try:
+#         return int(projects['weight'])
+#     except KeyError:
+#         return 0
 
-    in_project = next((p for p in projects if p['link'] == title), None)
-    in_exp = next((p for p in experiences if p['link'] == title), None)
 
-    if in_project is None and in_exp is None:
-        return render_template('404.html'), 404
-    # fixme: choose the experience one for now, cuz I've done some shite hardcoding here.
-    elif in_project is not None and in_exp is not None:
-        selected = in_exp
-    elif in_project is not None:
-        selected = in_project
-    else:
-        selected = in_exp
+# @app.route('/projects/<title>')
+# def project(title):
+#     projects = get_static_json("static/projects/projects.json")['projects']
+#     experiences = get_static_json("static/experiences/experiences.json")['experiences']
 
-    # load html if the json file doesn't contain a description
-    if 'description' not in selected:
-        path = "experiences" if in_exp is not None else "projects"
-        selected['description'] = io.open(get_static_file(
-            'static/%s/%s/%s.html' % (path, selected['link'], selected['link'])), "r", encoding="utf-8").read()
-    return render_template('project.html', common=common, project=selected)
+#     in_project = next((p for p in projects if p['link'] == title), None)
+#     in_exp = next((p for p in experiences if p['link'] == title), None)
+
+#     if in_project is None and in_exp is None:
+#         return render_template('404.html'), 404
+#     # fixme: choose the experience one for now, cuz I've done some shite hardcoding here.
+#     elif in_project is not None and in_exp is not None:
+#         selected = in_exp
+#     elif in_project is not None:
+#         selected = in_project
+#     else:
+#         selected = in_exp
+
+#     # load html if the json file doesn't contain a description
+#     if 'description' not in selected:
+#         path = "experiences" if in_exp is not None else "projects"
+#         selected['description'] = io.open(get_static_file(
+#             'static/%s/%s/%s.html' % (path, selected['link'], selected['link'])), "r", encoding="utf-8").read()
+#     return render_template('project.html', common=common, project=selected)
 
 
 @app.errorhandler(404)
